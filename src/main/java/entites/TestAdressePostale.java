9ebc246551adc92ae4c9e4df51412b4e06b8734b
leotex26 <leo.texier26@gmail.com>
2025-11-13 11:41:28 +0100

    tp2-instanciation

--- src/main/java/entites/TestAdressePostale.java
+++ src/main/java/entites/TestAdressePostale.java
@@ -3,8 +3,8 @@ package entites;
 public class TestAdressePostale {
   public static void main(String[] args) {
 
-    AdressePostale adresse1 = new AdressePostale(12, "Rue de la Paix", 75000, "Paris");
-    AdressePostale adresse2 = new AdressePostale(8, "Avenue des Champs-Élysées", 75008, "Paris");
+    AdressePostale adresse1 = new AdressePostale(12, "Rue de la soif", 35000, "Rennes");
+    AdressePostale adresse2 = new AdressePostale(8, "Avenue Charles de Gaulle", 35000, "Rennes");
 
     System.out.println(adresse1.ville + " - " + adresse1.libelleRue);
     System.out.println(adresse2.ville + " - " + adresse2.libelleRue);

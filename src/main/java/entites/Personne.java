1ef940d42dd56fc5540a2da1e0a29e6d98238096
leotex26 <leo.texier26@gmail.com>
2025-11-13 11:53:16 +0100

    tp3-instance et méthode

--- src/main/java/entites/Personne.java
+++ src/main/java/entites/Personne.java
@@ -1,9 +1,9 @@
 package entites;
 
 public class Personne {
-  public String nom;
-  public String prenom;
-  public AdressePostale adressePostale;
+  private String nom;
+  private String prenom;
+  private AdressePostale adressePostale;
 
 
   public Personne(String nom, String prenom) {
@@ -15,6 +15,47 @@ public class Personne {
     this.nom = nom;
     this.prenom = prenom;
     this.adressePostale = adressePostale;
+  }
+
+
+  //--------------------- FONCTIONS ----------------------------------------------------------------------
+
+  /*
+  * Afficher nom complet
+   */
+  public void afficherNomComplet() {
+    System.out.println(prenom + " " + nom.toUpperCase());
+  }
+
+  //--------------------- GETTERS ET SETTERS ----------------------------------------------------------------------
+
+  public void setNom(String nom) {
+    this.nom = nom;
+  }
+
+
+  public void setPrenom(String prenom) {
+    this.prenom = prenom;
+  }
+
+
+  public void setAdresse(AdressePostale adressePostale) {
+    this.adressePostale = adressePostale;
+  }
+
+
+  public String getNom() {
+    return nom;
+  }
+
+
+  public String getPrenom() {
+    return prenom;
+  }
+
+
+  public AdressePostale getAdresse() {
+    return adressePostale;
   }
 
 
--- src/main/java/entites/TestPersonne.java
+++ src/main/java/entites/TestPersonne.java
@@ -1,18 +1,25 @@
 package entites;
 
+import entites.Personne;
+
 public class TestPersonne {
   public static void main(String[] args) {
 
-    AdressePostale adresse1 = new AdressePostale(5,"Rue Victor Hugo",44000,"Nantes");
 
-    Personne personne1 = new Personne("Dupont","Marie");
-    personne1.adressePostale = adresse1;
+    Personne p1 = new Personne("Dupont", "Jean");
 
-    AdressePostale adresse2 = new AdressePostale(10,"Rue Nationale",59000,"Lille");
 
-    Personne personne2 = new Personne("Martin","Paul", adresse2);
+    AdressePostale adresse1 = new AdressePostale(25, "Rue des Lilas", 35000, "Rennes");
 
-    System.out.println(personne1.prenom + " " + personne1.nom + " habite à " + personne1.adressePostale.ville);
-    System.out.println(personne2.prenom + " " + personne2.nom + " habite à " + personne2.adressePostale.ville);
+
+    p1.setAdresse(adresse1);
+
+
+    p1.afficherNomComplet();
+
+    System.out.println("Adresse : " + p1.getAdresse().numeroRue + " "
+      + p1.getAdresse().libelleRue + ", "
+      + p1.getAdresse().codePostal + " "
+      + p1.getAdresse().ville);
   }
 }
